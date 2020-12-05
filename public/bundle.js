f4ddfbaae6434719439af6aa37069279ab352c25
Isaac <iyoon22@gmail.com>
2020-12-04 23:58:41 -0800

    Add Jeremiah 17:9 to level 6

--- public/bundle.js
+++ public/bundle.js
@@ -169,7 +169,7 @@ const verses = {
   3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them.',
   4 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
   5 : 'The fear of the LORD is the beginning of knowledge, but fools despise wisdom and instruction.',
-  6 : 'level 6',
+  6 : 'The heart is deceitful above all things and beyond cure. Who can understand it?',
   7 : 'level 7',
   8 : 'level 8',
   9 : 'level 9',
