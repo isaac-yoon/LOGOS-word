18a902780457ce97da7aa3a85760adae16180519
Isaac <iyoon22@gmail.com>
2020-12-04 23:53:24 -0800

    Add Proverbs 1:7 to level 5

--- public/bundle.js
+++ public/bundle.js
@@ -165,10 +165,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
-  2 : 'In the beginning was the Word, and the Word was with God, and the Word was God',
-  3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them',
+  2 : 'In the beginning was the Word, and the Word was with God, and the Word was God.',
+  3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them.',
   4 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
-  5 : 'level 5',
+  5 : 'The fear of the LORD is the beginning of knowledge, but fools despise wisdom and instruction.',
   6 : 'level 6',
   7 : 'level 7',
   8 : 'level 8',
