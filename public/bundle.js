26e974f0a50d2a16f6c019c5147849fbe975475b
Isaac <iyoon22@gmail.com>
2020-12-04 23:51:39 -0800

    Add Micah 6:8 to level 4

--- public/bundle.js
+++ public/bundle.js
@@ -167,7 +167,7 @@ const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'In the beginning was the Word, and the Word was with God, and the Word was God',
   3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them',
-  4 : 'level 4',
+  4 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
   5 : 'level 5',
   6 : 'level 6',
   7 : 'level 7',
