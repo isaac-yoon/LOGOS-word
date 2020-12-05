41a3c8be972a053703bd09575d7566c9da43666a
Isaac <iyoon22@gmail.com>
2020-12-04 23:43:41 -0800

    Add Genesis 1:27 to level 3

--- public/bundle.js
+++ public/bundle.js
@@ -166,6 +166,13 @@ __webpack_require__.r(__webpack_exports__);
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'In the beginning was the Word, and the Word was with God, and the Word was God',
+  3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them',
+  4 : 'level 4',
+  5 : 'level 5',
+  6 : 'level 6',
+  7 : 'level 7',
+  8 : 'level 8',
+  9 : 'level 9',
 };
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (verses);
--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,6 +1,13 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'In the beginning was the Word, and the Word was with God, and the Word was God',
+  3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them',
+  4 : 'level 4',
+  5 : 'level 5',
+  6 : 'level 6',
+  7 : 'level 7',
+  8 : 'level 8',
+  9 : 'level 9',
 };
 
 export default verses;
