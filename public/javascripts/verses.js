4961eb570909036a69aac7df62c285d98f157094
Isaac <iyoon22@gmail.com>
2020-12-13 20:51:32 -0800

    Add 1 Corinthians 13:12

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -7,8 +7,9 @@ const verses = {
   6 : 'The heart is deceitful above all things and beyond cure. Who can understand it?',
   7 : 'But seek first his kingdom and his righteousness, and all these things will be given to you as well.',
   8 : "Now faith is confidence in what we hope for and assurance about what we do not see. By faith we understand that the universe was formed at God's command, so that what is seen was not made out of what was visible.",
-  9 : 'Then you will know the truth, and the truth will set you free.',
-  10 : 'these are the things God has revealed to us by his Spirit. The Spirit searches all things, even the deep things of God.',
+  9 : "For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.",
+  10 : 'Then you will know the truth, and the truth will set you free.',
+  11 : 'these are the things God has revealed to us by his Spirit. The Spirit searches all things, even the deep things of God.',
 }
 
 export default verses;
