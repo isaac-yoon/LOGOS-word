bddabd001be8368d5b5a81d4a949f0d36d1c67aa
Isaac <iyoon22@gmail.com>
2022-04-20 12:55:49 -0700

    Edit verses

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,17 +1,11 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'In the beginning was the Word, and the Word was with God, and the Word was God.',
-  3 : 'So God created mankind in his own image, in the image of God he created them; male and female he created them.',
-  4 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
-  5 : 'The fear of the LORD is the beginning of knowledge, but fools despise wisdom and instruction.',
-  6 : 'The heart is deceitful above all things and desperately wicked: who can know it?',
-  7 : 'But seek first his kingdom and his righteousness, and all these things will be given to you as well.',
-  8 : "Now faith is confidence in what we hope for and assurance about what we do not see. By faith we understand that the universe was formed at God's command, so that what is seen was not made out of what was visible.",
-  9 : "For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.",
-  10 : 'Then you will know the truth, and the truth will set you free.',
-  11 : 'these are the things God has revealed to us by his Spirit. The Spirit searches all things, even the deep things of God.',
-  12 : 'Trust in the LORD with all thine heart; and lean not unto thine own understanding.',
-  13 : 'Amen.'
+  3 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
+  4 : "Now faith is confidence in what we hope for and assurance about what we do not see. By faith we understand that the universe was formed at God's command, so that what is seen was not made out of what was visible.",
+  5 : "For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.",
+  6 : 'Then you will know the truth, and the truth will set you free.',
+  7 : 'these are the things God has revealed to us by his Spirit. The Spirit searches all things, even the deep things of God.',
 }
 
 export default verses;
