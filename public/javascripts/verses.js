dd16cad7d134e8b71315787f0f86e644d069cdf3
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2024-08-27 03:49:57 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,11 +1,11 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
-  2 : 'In the beginning was the Word, and the Word was with God, and the Word was God.',
-  3 : 'He has shown you, O mortal, what is good. And what does the LORD require of you? To act justly and to love mercy and to walk humbly with your God.',
-  4 : "Now faith is confidence in what we hope for and assurance about what we do not see. By faith we understand that the universe was formed at God's command, so that what is seen was not made out of what was visible.",
-  5 : "For now we see only a reflection as in a mirror; then we shall see face to face. Now I know in part; then I shall know fully, even as I am fully known.",
-  6 : 'Then you will know the truth, and the truth will set you free.',
-  7 : 'these are the things God has revealed to us by his Spirit. The Spirit searches all things, even the deep things of God.',
+  2 : 'Pour out thy fury upon the heathen that know thee not, and upon the families that call not on thy name...',
+  3 : '',
+  4 : '',
+  5 : '',
+  6 : '',
+  7 : '',
 }
 
 export default verses;
