ba10fdb3f4eb0aa014fdabc5f4756ad17d9e006f
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2024-08-30 07:53:30 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,11 +1,11 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'Pour out thy fury upon the heathen that know thee not, and upon the families that call not on thy name...',
-  3 : '',
-  4 : '',
-  5 : '',
-  6 : '',
-  7 : '',
+  3 : '...Cursed is the man that trusteth in man, and maketh flesh his arm, and whose heart departed from...',
+  4 : 'Then I testified against them, and said unto them, Why lodge ye about the wall? if ye do so again, I will lay hands on you. From that time forth came they no more on the sabbath.',
+  5 : 'For without are dogs, and sorcerers, and whoremongers, and murderers, and idolaters, and whosoever loveth and maketh a lie.',
+  6 : 'I am about to pour out my wrath on you and spend my anger against you. I will judge you according to your conduct and repay you for all your detestable practices.',
+  7 : 'Seek ye Jehovah while he may be found; call ye upon him while he is near:',
 }
 
 export default verses;
