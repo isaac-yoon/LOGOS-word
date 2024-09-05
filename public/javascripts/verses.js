0c8e03b19fbcb5cd96a2dd285b97e39bce198df9
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2024-09-05 12:26:22 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -6,6 +6,12 @@ const verses = {
   5 : 'For without are dogs, and sorcerers, and whoremongers, and murderers, and idolaters, and whosoever loveth and maketh a lie.',
   6 : 'I am about to pour out my wrath on you and spend my anger against you. I will judge you according to your conduct and repay you for all your detestable practices.',
   7 : 'Seek ye Jehovah while he may be found; call ye upon him while he is near:',
+  8 : 'More numerous than the hairs of my head are those who hate me without reason; many are those who would destroy me, my treacherous enemies',
+  9 : 'For they persecute those You have struck; they talk about the pain of those You have felled.',
+  10 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
+  11 : 'Amen. 아멘.",
+  12 : 'For God will deliver Zion and rebuild the cities of Judah; they shall live there and inherit it; the offspring of His servants shall possess it; those who cherish His name shall dwell there.',
+  13 : 'Even so, come quickly, Lord Jesus.',
 }
 
 export default verses;
