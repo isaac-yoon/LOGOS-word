6821e9cdbed8e8156e8f9bd465e6ab1b4d685053
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-02-23 21:36:20 -0800

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,17 +1,17 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
   2 : 'Pour out thy fury upon the heathen that know thee not, and upon the families that call not on thy name...',
-  3 : '...Cursed is the man that trusteth in man, and maketh flesh his arm, and whose heart departed from...',
+  3 : 'Thus saith the LORD; Cursed be the man that trusteth in man, and maketh flesh his arm, and whose heart departed from the LORD.',
   4 : 'Then I testified against them, and said unto them, Why lodge ye about the wall? if ye do so again, I will lay hands on you. From that time forth came they no more on the sabbath.',
   5 : 'For without are dogs, and sorcerers, and whoremongers, and murderers, and idolaters, and whosoever loveth and maketh a lie.',
   6 : 'I am about to pour out my wrath on you and spend my anger against you. I will judge you according to your conduct and repay you for all your detestable practices.',
-  7 : 'Seek ye Jehovah while he may be found; call ye upon him while he is near:',
-  8 : 'More numerous than the hairs of my head are those who hate me without reason; many are those who would destroy me, my treacherous enemies',
-  9 : 'For they persecute those You have struck; they talk about the pain of those You have felled.',
-  10 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
-  11 : 'Amen. 아멘.",
+  7 : 'Because with lies ye have grieved the heart of the righteous, whom I have not made sad, and strengthened the hands of the wicked, that he should not return from his wicked way, and be saved alive; therefore ye shall no more see false visions nor divine divinations: and I will deliver my people out of your hand; and ye shall know that I am Jehovah.',
+  8 : 'Seek ye Jehovah while he may be found; call ye upon him while he is near:',
+  9 : 'More numerous than the hairs of my head are those who hate me without reason; many are those who would destroy me, my treacherous enemies',
+  10 : 'For they persecute those You have struck; they talk about the pain of those You have felled.',
+  11 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
   12 : 'For God will deliver Zion and rebuild the cities of Judah; they shall live there and inherit it; the offspring of His servants shall possess it; those who cherish His name shall dwell there.',
-  13 : 'Even so, come quickly, Lord Jesus.',
+  13 : 'Brethren, the grace of our Lord Jesus Christ be with your spirit. Amen.',
 }
 
 export default verses;
