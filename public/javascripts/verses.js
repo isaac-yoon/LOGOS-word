26a77d23eb812130dd3e4ee67d0e1a33c79f8943
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-03-12 16:58:48 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -6,7 +6,7 @@ const verses = {
   5 : 'Because with lies ye have grieved the heart of the righteous, whom I have not made sad, and strengthened the hands of the wicked, that he should not return from his wicked way, and be saved alive; therefore ye shall no more see false visions nor divine divinations: and I will deliver my people out of your hand; and ye shall know that I am Jehovah.',
   6 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
   7 : 'For God will deliver Zion and rebuild the cities of Judah; they shall live there and inherit it; the offspring of His servants shall possess it; those who cherish His name shall dwell there.',
-  8 : 'extending kindness to the thousandth generation, forgivin iniquity, trasngression, and sin; yet He does not remit all punishment, yet He does not clear the guilty but visits the iniquity of parents upon children and children's children, upon the third and fourth generations.',
+  8 : 'extending kindness to the thousandth generation, forgiving iniquity, trasngression, and sin; yet He does not remit all punishment, yet He does not clear the guilty but visits the iniquity of parents upon children and children's children, upon the third and fourth generations.',
   9 : 'Brethren, the grace of our Lord Jesus Christ be with your spirit. Amen.',
 }
 
