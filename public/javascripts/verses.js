a34ae685e8977b65c0256b5d7cf3a701b6748304
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-11 04:45:59 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,15 +1,8 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
-  2 : 'Thus saith the LORD; Cursed be the man that trusteth in man, and maketh flesh his arm, and whose heart departed from the LORD.',
-  4 : 'I am about to pour out my wrath on you and spend my anger against you. I will judge you according to your conduct and repay you for all your detestable practices.',
-  5 : 'Because with lies ye have grieved the heart of the righteous, whom I have not made sad, and strengthened the hands of the wicked, that he should not return from his wicked way, and be saved alive; therefore ye shall no more see false visions nor divine divinations: and I will deliver my people out of your hand; and ye shall know that I am Jehovah.',
   6 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
   7 : 'For God will deliver Zion and rebuild the cities of Judah; they shall live there and inherit it; the offspring of His servants shall possess it; those who cherish His name shall dwell there.',
-  8 : 'extending kindness to the thousandth generation, forgiving iniquity, trasngression, and sin; yet He does not remit all punishment, yet He does not clear the guilty but visits the iniquity of parents upon children and children's children, upon the third and fourth generations.',
   9 : 'The Lord doth build up Jerusalem: he gathereth together the outcasts of Israel. He healeth the broken in heart, and bindeth up their wounds. He telleth the number of the stars; he calleth them all by their names. Great is our Lord, and of great power: his understanding is infinite. The Lord lifteth up the meek: he casteth the wicked down to the ground. He giveth to the beast his food, and to the young ravens which cry. He delighteth not in the strength of the horse: he taketh not pleasure in the legs of a man. For he hath strengthened the bars of thy gates; he hath blessed thy children within thee. He maketh peace in thy borders, and filleth thee with the finest of the wheat. He sendeth forth his commandment upon earth: his word runneth very swiftly. He sendeth out his word, and melteth them: he causeth his wind to blow, and the waters flow. He sheweth his word unto Jacob, his statutes and his judgments unto Israel.'
-  10 : 'I have observed the task which God has given the sons of man to be concerned with: He made everything beautiful in its time; He has also put an enigma into their minds so that man cannot comprehend what God has done from beginning to end. Thus I perceived that there is nothing better for them than to rejoice and do good in his life.',
-  11 : 'Pure religion and undefiled before our God and Father is this, to visit the fatherless and widows in their affliction, and to keep oneself unspotted from the world.',  
-  12 : 'Brethren, the grace of our Lord Jesus Christ be with your spirit. Amen.',
 }
 
 export default verses;
