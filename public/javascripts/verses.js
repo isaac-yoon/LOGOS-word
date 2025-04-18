b38ee8073c0f7c5be0a9f4af709dadc1d9371058
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-18 15:11:22 -0700

    Update verses.js

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,7 +1,5 @@
 const verses = {
   1 : 'In the beginning God created the heavens and the earth.',
-  6 : 'Add that to their guilt; let them have no share of Your beneficience; may they be erased from the book of life, and not be inscribed with the righteous.',
-  7 : 'For God will deliver Zion and rebuild the cities of Judah; they shall live there and inherit it; the offspring of His servants shall possess it; those who cherish His name shall dwell there.',
   9 : 'The Lord doth build up Jerusalem: he gathereth together the outcasts of Israel. He healeth the broken in heart, and bindeth up their wounds. He telleth the number of the stars; he calleth them all by their names. Great is our Lord, and of great power: his understanding is infinite. The Lord lifteth up the meek: he casteth the wicked down to the ground. He giveth to the beast his food, and to the young ravens which cry. He delighteth not in the strength of the horse: he taketh not pleasure in the legs of a man. For he hath strengthened the bars of thy gates; he hath blessed thy children within thee. He maketh peace in thy borders, and filleth thee with the finest of the wheat. He sendeth forth his commandment upon earth: his word runneth very swiftly. He sendeth out his word, and melteth them: he causeth his wind to blow, and the waters flow. He sheweth his word unto Jacob, his statutes and his judgments unto Israel.'
 }
 
