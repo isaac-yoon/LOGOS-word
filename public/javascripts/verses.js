2c98489d9202f45a15616c7d61f22ed84b186662
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-20 05:51:39 -0700

    Update verses.js
    000

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,6 +1,4 @@
 const verses = {
-  1 : 'In the beginning God created the heavens and the earth.',
-  9 : 'The Lord doth build up Jerusalem: he gathereth together the outcasts of Israel. He healeth the broken in heart, and bindeth up their wounds. He telleth the number of the stars; he calleth them all by their names. Great is our Lord, and of great power: his understanding is infinite. The Lord lifteth up the meek: he casteth the wicked down to the ground. He giveth to the beast his food, and to the young ravens which cry. He delighteth not in the strength of the horse: he taketh not pleasure in the legs of a man. For he hath strengthened the bars of thy gates; he hath blessed thy children within thee. He maketh peace in thy borders, and filleth thee with the finest of the wheat. He sendeth forth his commandment upon earth: his word runneth very swiftly. He sendeth out his word, and melteth them: he causeth his wind to blow, and the waters flow. He sheweth his word unto Jacob, his statutes and his judgments unto Israel.'
 }
 
 export default verses;
