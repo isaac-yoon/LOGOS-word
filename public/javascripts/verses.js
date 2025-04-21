aa0c38bf442d436510e4087aa973814b33fb64c4
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-21 12:57:03 -0700

    Update verses.js
    Signed-off-by: Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,4 +1,9 @@
 const verses = {
+  5 : 'Thus saith the LORD; Cursed be the man that trusteth in man, and maketh flesh his arm, and whose heart departeth from the LORD.',
+  6 : 'For he shall be like the heath in th desert, and shall not see when good cometh; but shall inhabit the parched places in th wilderness, in a salt land and not inhabited.',
+  7 : 'Blessed is the man that trusteth in the LORD, and whose hope the LORD is.',
+  8 : 'For he shall be as a tree planted by the waters, and that spreadeth out her roots by the river, and shall not see when heat cometh, but her leaf shall be green; and shall not be careful in the year of drought, neither shall cease from yielding fruit.', 
+  
 }
 
 export default verses;
