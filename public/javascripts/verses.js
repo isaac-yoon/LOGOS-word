2c28f5b9e13e917a461ebb017af7743a31c58068
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-22 08:00:50 -0700

    Update verses.js
    Signed-off-by: Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -3,7 +3,11 @@ const verses = {
   6 : 'For he shall be like the heath in th desert, and shall not see when good cometh; but shall inhabit the parched places in th wilderness, in a salt land and not inhabited.',
   7 : 'Blessed is the man that trusteth in the LORD, and whose hope the LORD is.',
   8 : 'For he shall be as a tree planted by the waters, and that spreadeth out her roots by the river, and shall not see when heat cometh, but her leaf shall be green; and shall not be careful in the year of drought, neither shall cease from yielding fruit.', 
-  
+  1 : '',
+  2 : '',
+  3 : '',
+  4 : '',
+  9 : '',
 }
 
 export default verses;
