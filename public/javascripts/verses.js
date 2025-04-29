dbf336ad075460cbaf8fbed114cfd9de75b2cd9a
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-04-28 17:51:28 -0700

    Update verses.js
    Signed-off-by: Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -3,11 +3,11 @@ const verses = {
   6 : 'For he shall be like the heath in th desert, and shall not see when good cometh; but shall inhabit the parched places in th wilderness, in a salt land and not inhabited.',
   7 : 'Blessed is the man that trusteth in the LORD, and whose hope the LORD is.',
   8 : 'For he shall be as a tree planted by the waters, and that spreadeth out her roots by the river, and shall not see when heat cometh, but her leaf shall be green; and shall not be careful in the year of drought, neither shall cease from yielding fruit.', 
-  1 : '',
-  2 : '',
-  3 : '',
-  4 : '',
-  9 : '',
+  1 : 'Professing themselves to be wise, they became fools,',
+  2 : 'Being filled with an unrighteousness, fornication, wickedness, covetousness, maliciousness; full of envy, murder, debate, deceit, malignity; whisperers',
+  3 : 'Backbiters, haters of God, despiteful, proud, boasters, inventors of evil things, disobedient to parents,',
+  4 : 'Without understanding, covenantbreakers, 11 without natural affection, implacable, unmerciful:',
+  9 : 'Who knowing the judgment of God, that they which commit such things are worthy of death, not only do the same, but have 12 pleasure in them that do them.',
 }
 
 export default verses;
